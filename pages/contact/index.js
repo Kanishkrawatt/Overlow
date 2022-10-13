94c21b7ad3a71c97bfe63a891bfc074291935c54
Kanishk <rawat27kanishk@gmail.com>
2022-10-13 18:00:49 +0530

    Replace useState with useRef

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,22 +1,22 @@
-import React, { useState } from 'react'
+import React, { useRef } from 'react'
 import axios from 'axios';
 
 function contact() {
   
-  const [FirstName, setFirstName] = useState("");
-  const [LastName,setLastName] = useState("");
-  const [Email, setEmail] = useState("");
-  const [Message, setMessage] = useState("");
+  const FirstName = useRef();
+  const LastName = useRef();
+  const Email = useRef();
+  const Message = useRef();
 
   function submitfunc(e){
       e.preventDefault();
-      let slug = `${FirstName}-${LastName}-${Email}`
-      let data = {FirstName,LastName,Email,Message,slug};
+      let slug = `${FirstName.current.value}-${LastName.current.value}-${Email.current.value}`
+      let data = {"FirstName":FirstName.current.value,"LastName":LastName.current.value,"Email":Email.current.value,"Message":Message.current.value,slug};
       axios.post('/api/contact',data);
-      setFirstName("");
-      setEmail("");
-      setLastName("");
-      setMessage("");
+      FirstName.current.value="";
+      LastName.current.value="";
+      Email.current.value="";
+      Message.current.value="";
 }
   
   return (<>
@@ -26,13 +26,13 @@ function contact() {
         <label className="block uppercase tracking-wide text-gray-700 text-xs   mb-2" htmlFor="grid-first-name">
           First Name
         </label>
-        <input className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text"  value={FirstName} onChange={(e)=>setFirstName(e.target.value)}/>
+        <input className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text"  ref={FirstName}/>
       </div>
       <div className="w-full md:w-1/2 px-3">
         <label className="block uppercase tracking-wide text-gray-700 text-xs   mb-2" htmlFor="grid-last-name">
           Last Name
         </label>
-        <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text"  value={LastName} onChange={(e)=>setLastName(e.target.value)}/>
+        <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" ref={LastName} />
       </div>
     </div>
     <div className="flex flex-wrap -mx-3 mb-6">
@@ -40,7 +40,7 @@ function contact() {
         <label className="block uppercase tracking-wide text-gray-700 text-xs   mb-2" htmlFor="grid-password">
           E-mail
         </label>
-        <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="email" type="email" value={Email} onChange={(e)=>setEmail(e.target.value)} />
+        <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="email" type="email" ref={Email} />
       </div>
     </div>
     <div className="flex flex-wrap -mx-3 mb-6">
@@ -48,7 +48,7 @@ function contact() {
         <label className="block uppercase tracking-wide text-gray-700 text-xs   mb-2" htmlFor="grid-password">
           Message
         </label>
-        <textarea className=" no-resize appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none" id="message" value={Message} onChange={(e)=>setMessage(e.target.value)} ></textarea>
+        <textarea className=" no-resize appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none" id="message" ref={Message} ></textarea>
       </div>
     </div>
     <div className="md:flex md:items-center">
